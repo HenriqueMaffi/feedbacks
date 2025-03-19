3ba0cd3975698e30e41feb335200b9d4ef30d3a7
Henrique Maffi <henrique.maffi@gmail.com>
2025-03-19 19:45:10 -0300

    componente dashitem

---
+++ components/dashboard/DashItem.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="lg:h-[150px] p-4 xl:p-6 rounded-xl shadow-md bg-gray-100 relative hover:bg-white hover:shadow-lg transition-[shadow_color] duration-300">
+    <slot />
+  </div>
+</template>
